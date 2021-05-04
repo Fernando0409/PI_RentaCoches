558ac1693bc0b9a403093fbae49e7321a17a54ad
Luis Fernando <fernando04_software@outlook.com>
2021-05-03 21:42:39 -0500

    Registrar usuario - maquetacion
    Esta al 85%, contiene todos los elementos del formulario y su barra lateral. Se añaden los archivos css y js para su dinamismo
    
    Nota: Solo falta que cuando se abra el menu no desaparezca

--- webSite/js/main.js
+++ webSite/js/main.js
@@ -3,3 +3,19 @@ const btnToogle = document.querySelector('.toggle-btn');
 btnToogle.addEventListener('click', function() {
     document.getElementById("sidebar").classList.toggle('active');
 })
+
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
